b70386ba049de7a9ef14cacfe16bc59c7f87b869
Tai Dunn <39572984+taidunn@users.noreply.github.com>
2019-10-22 13:21:35 -0400

    Update RunningClass.java

--- RunningClass.java
+++ RunningClass.java
@@ -1,1 +1,53 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
+
+
+
+public class RunningClass {
+	
+	private static int num;
+	private static String mainInfo;
+	
+
+	public static void main(String[] args) {
+		Date date1 = new Date();
+		Input askForInput = new Input();
+		num = askForInput.promptForInput();
+		//promptForInput();
+		//mainInfo = askForInput.promptForString();
+
+	
+		switch (num) {
+		case 1:
+			//Make a new employee
+			//Run the class for this
+			//promptForInput();
+			mainInfo = askForInput.promptForString();
+			System.out.println(mainInfo);
+			/*enter Employee iD
+			Enter
+			System.out.println(1);
+			Employee Tai = new Employee("1234", "Tai");
+			*/
+			break;
+		case 2:
+			//Edit Employee
+			System.out.println("test");
+			askForInput.promptForInput();
+			break;
+		case 3:
+			//Print Table
+			//promptForInput();
+			System.out.println(3);
+			break;
+		case 4:
+			//Delete/Hide User
+			//promptForInput();
+			System.out.println(4);
+			break;
+		default:
+			break;
+			}
+		}
+	}
