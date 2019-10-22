3e570f1667c95006ef96542d0951086fb7793643
Tai Dunn <39572984+taidunn@users.noreply.github.com>
2019-10-22 13:21:19 -0400

    Update Date.java

--- Date.java
+++ Date.java
@@ -1,1 +1,12 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+public class Date {
+	public Date() {
+	    LocalDateTime myDateObj = LocalDateTime.now();
+	    DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
+	    String LocalInformation = myDateObj.format(myFormatObj);
+	    System.out.println("Hello, today is " + LocalInformation + " please see below to take the next steps on what you need to do."); // Display the current date"
+	}
+	
+}
