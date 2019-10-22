014604c0c2bccb5645e2c1bfca40ea1cec019a73
Tai Dunn <39572984+taidunn@users.noreply.github.com>
2019-10-22 16:24:34 -0400

    Create Placeholder.java

---
+++ Placeholder.java
@@ -0,0 +1,18 @@
+
+/*public class Placeholder {
+	String classname;
+	String date;
+	
+	public Placeholder(String classname, String date) {
+		this.classname = classname;
+		this.date = date;
+	}
+	
+	public String getClassName() {
+		return classname;
+	}
+	
+	public void setClassName(String classname) {
+		this.classname = classname;
+	}
+}*/
