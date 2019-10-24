9889cf389b5c43e4e5e4c66a75ca1a550bad83aa
Tai Dunn <39572984+taidunn@users.noreply.github.com>
2019-10-24 16:10:04 -0400

    Update RunningClass.java

--- RunningClass.java
+++ RunningClass.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.FileWriter;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
 import java.io.FileReader;
 
 
@@ -11,12 +16,18 @@ public class RunningClass {
 
 
 	public static void main(String[] args) {
+		LatencyHelp progress = new LatencyHelp();
+		progress.bar();
 		Disclaimer disclaimer1 = new Disclaimer();
 		disclaimer1.unknown();
 		Date date1 = new Date();
 		Input askForInput = new Input();
 		EditingEmployee editEmployee = new EditingEmployee();
-		ArrayList<Employee> EmployeeList = new ArrayList<>();
+		DeleteEmployee deleteEmployee = new DeleteEmployee();
+		
+		
+		
+		ArrayList<Employee> employeeList = new ArrayList<>();
 		Scanner reader = new Scanner(System.in);  // Reading from System.in
 		do {
 			System.out.println("\n1 - Make a new employee. \n2 - Edit an Employee. \n3 - Print the Table \n4 - Delete/Hide an Employee");
@@ -29,24 +40,26 @@ public class RunningClass {
 		case 1:
 			//Make a new employee
 			//Run the class for this
-			mainInfo = askForInput.promptForString(EmployeeList);
+			mainInfo = askForInput.promptForString(employeeList);
 			System.out.println(mainInfo);
 			break;
 		case 2:
 			//Edit Employee
-			editEmployee.EditingEmployee(EmployeeList);
+			editEmployee.EditingEmployee(employeeList);
 			System.out.println(mainInfo);
 		case 3:
 			//Print Table
 			//promptForInput();
 			System.out.println(3);
 			System.out.println("EmployeeID || ");
-			System.out.println(EmployeeList);
+			System.out.println(employeeList);
 			break;
 		case 4:
 			//Delete/Hide User 
 			//promptForInput();
+			deleteEmployee.DeleteEmployee(employeeList);
 			System.out.println(4);
+			
 			break;
 		default:
 			break;
