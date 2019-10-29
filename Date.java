f4db05a8b8e4ea3f93d7db196b39d0254e6ba2cb
Tai Dunn <39572984+taidunn@users.noreply.github.com>
2019-10-29 16:19:25 -0400

    Update Date.java

--- Date.java
+++ Date.java
@@ -1,12 +1,13 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+//The purpose of this program is to give you the real time when you open the program.
 public class Date {
 	public Date() {
 	    LocalDateTime myDateObj = LocalDateTime.now();
 	    DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
 	    String LocalInformation = myDateObj.format(myFormatObj);
-	    System.out.println("\n\nHello, today is " + LocalInformation + " please continue below."); // Display the current date"
+	    System.out.println("\n\nHello, today is " + LocalInformation + " please continue below.\n"); 
 	}
 	
 }
