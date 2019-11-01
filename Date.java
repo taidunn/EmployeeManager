d24a9659d488c97b65339d475e030fe0d8ecf8e9
Tai Dunn <39572984+taidunn@users.noreply.github.com>
2019-11-01 13:53:21 -0400

    Update Date.java

--- Date.java
+++ Date.java
@@ -2,6 +2,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 //The purpose of this program is to give you the real time when you open the program.
+
 public class Date {
 	public Date() {
 	    LocalDateTime myDateObj = LocalDateTime.now();
