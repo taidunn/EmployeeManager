6b139a8a21f5c6b318301f99021389c8af99ecfe
Tai Dunn <39572984+taidunn@users.noreply.github.com>
2019-11-01 13:57:09 -0400

    Update WriteExcelMain.java

--- WriteExcelMain.java
+++ WriteExcelMain.java
@@ -1,58 +1,113 @@
-/*import java.io.File;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
  
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class WriteExcelMain {
  
- public static void main(String[] args) throws IOException {
-	 new WriteExcelMain().writeFileUsingPOI();
-   }
- 
- public void writeFileUsingPOI() throws IOException 
- {
+ public void writeFileUsingPOI(ArrayList < Employee > employeeList) throws IOException {
   //create blank workbook
   XSSFWorkbook workbook = new XSSFWorkbook(); 
  
   //Create a blank sheet
-  XSSFSheet sheet = workbook.createSheet("Country");
+  XSSFSheet sheet = workbook.createSheet("Management");
  
-//  ArrayList<Object[]> data=new ArrayList<Object[]>();
-  ArrayList<Employee> employeeList = new ArrayList<>();
  
   //Iterate over data and write to sheet
   int rownum = 0;
-  for (Employee person : employeeList)
-  {
+  for (Employee person : employeeList){
    Row row = sheet.createRow(rownum++);
  
    int cellnum = 0;
-   for (Object obj : person)
-   {
-    Cell cell = row.createCell(cellnum++);
-    if(obj instanceof String)
-     cell.setCellValue((String)obj);
-    else if(obj instanceof Double)
-     cell.setCellValue((Double)obj);
-    else if(obj instanceof Integer)
-     cell.setCellValue((Integer)obj);
-   }
+   Cell cell;
+   CellStyle cellStyle = workbook.createCellStyle();
+   CreationHelper createHelper = workbook.getCreationHelper();
+   cellStyle.setDataFormat(createHelper.createDataFormat().getFormat("dd-mmm-yy"));
+   cell = row.createCell(cellnum++);
+   
+    cell.setCellStyle(cellStyle);
+    cell = row.createCell(cellnum++); 
+    cell.setCellValue(person.getEmployeeId());
+    
+    cell.setCellStyle(cellStyle);
+    cell = row.createCell(cellnum++);
+    cell.setCellValue(person.getFName());
+    
+    cell.setCellStyle(cellStyle);
+    cell = row.createCell(cellnum++);
+    cell.setCellValue(person.getLName());
+    
+    cell.setCellStyle(cellStyle);
+    cell = row.createCell(cellnum++);   
+    cell.setCellValue(person.getClass1());
+    
+    cell.setCellStyle(cellStyle);
+    cell = row.createCell(cellnum++);
+    cell.setCellValue(person.getClass2());
+    
+    cell.setCellStyle(cellStyle);
+    cell = row.createCell(cellnum++);
+    cell.setCellValue(person.getClass3());
+    
+    cell.setCellStyle(cellStyle);
+    cell = row.createCell(cellnum++);
+    cell.setCellValue(person.getClass4());
+    
+    cell.setCellStyle(cellStyle);
+    cell = row.createCell(cellnum++);
+    cell.setCellValue(person.getClass5());
+    
+    cell.setCellStyle(cellStyle);
+    cell = row.createCell(cellnum++);
+    cell.setCellValue(person.getClass6());
+    
+    cell.setCellStyle(cellStyle);
+    cell = row.createCell(cellnum++);
+    cell.setCellValue(person.getClass7());
+    
+    cell.setCellStyle(cellStyle);
+    cell = row.createCell(cellnum++);
+    cell.setCellValue(person.getClass8());
+    
+    cell.setCellStyle(cellStyle);
+    cell = row.createCell(cellnum++);
+    cell.setCellValue(person.getClass9());
+    
+    cell.setCellStyle(cellStyle);
+    cell = row.createCell(cellnum++);
+    cell.setCellValue(person.getClass10());
+    
+    cell.setCellStyle(cellStyle);
+    cell = row.createCell(cellnum++);
+    cell.setCellValue(person.getClass11());
+    
+    cell.setCellStyle(cellStyle);
+    cell = row.createCell(cellnum++);
+    cell.setCellValue(person.getClass12());
+    
+    cell = row.createCell(cellnum++);
+    cell.setCellValue(person.getClass13());
+    
+    cell = row.createCell(cellnum++);
+    cell.setCellValue(person.getClass14());
+    
+    cell = row.createCell(cellnum++);
+    cell.setCellValue(person.getClass15());
   }
-  try
-  {
+  try {
    //Write the workbook in file system
-   FileOutputStream out = new FileOutputStream(new File("CountriesDetails.xlsx"));
+   FileOutputStream out = new FileOutputStream(new File("EmployeeManager.xlsx"));
    workbook.write(out);
    out.close();
-   System.out.println("CountriesDetails.xlsx has been created successfully");
-  } 
-  catch (Exception e) 
-  {
+   System.out.println("EmployeeManager.xlsx has been created successfully");
+  }catch (Exception e){
    e.printStackTrace();
   }
   finally {
@@ -61,4 +116,4 @@ public class WriteExcelMain {
  }
  
  
-}*/
+}
