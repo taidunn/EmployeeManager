75ea4d00aa5e04398131a165f3a52bfad1289e70
Tai Dunn <39572984+taidunn@users.noreply.github.com>
2019-11-08 13:57:26 -0500

    Create Banner.java

---
+++ Banner.java
@@ -0,0 +1,8 @@
+public class Banner {
+	public Banner() {
+		System.out.println("###################################################"); 
+		System.out.println("#     **** THIS IS A PLACEHOLDER FOR NOW ****     #"); 
+		System.out.println("###################################################");
+	}
+}
+ 
