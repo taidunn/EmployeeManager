55f7a7b732e764ead14875401523240aad75e4c0
Tai Dunn <39572984+taidunn@users.noreply.github.com>
2019-11-08 14:01:43 -0500

    Update WriteExcelMain.java

--- WriteExcelMain.java
+++ WriteExcelMain.java
@@ -1,18 +1,23 @@
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
  
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFFormulaEvaluator;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class WriteExcelMain {
  
- public void writeFileUsingPOI(ArrayList < Employee > employeeList) throws IOException {
+ public void writeFileUsingPOI(ArrayList < Employee > employeeList) throws IOException, ParseException {
   //create blank workbook
   XSSFWorkbook workbook = new XSSFWorkbook(); 
  
@@ -113,7 +118,9 @@ public class WriteExcelMain {
    CellStyle cellStyle = workbook.createCellStyle();
    CreationHelper createHelper = workbook.getCreationHelper();
    cellStyle.setDataFormat(createHelper.createDataFormat().getFormat("dd-mmm-yy"));
-
+   cellStyle.setFillForegroundColor(IndexedColors.GREEN.getIndex());  
+   cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+   
    
    	//Entering for Work
     cell = row.createCell(cellnum); 
@@ -131,98 +138,113 @@ public class WriteExcelMain {
     sheet.autoSizeColumn(cellnum);
     cellnum++;
     //Entering for Class1
+    cell = row.createCell(cellnum);
     cell.setCellStyle(cellStyle);
-    cell = row.createCell(cellnum);   
+    
     cell.setCellValue(person.getClass1());
     sheet.autoSizeColumn(cellnum);
     cellnum++;
     //Entering for Class2    
-    cell.setCellStyle(cellStyle);
     cell = row.createCell(cellnum);
+    cell.setCellStyle(cellStyle);
     cell.setCellValue(person.getClass2());
     sheet.autoSizeColumn(cellnum);
     cellnum++;
     //Entering for Class3
-    cell.setCellStyle(cellStyle);
     cell = row.createCell(cellnum);
+    cell.setCellStyle(cellStyle);
     cell.setCellValue(person.getClass3());
     sheet.autoSizeColumn(cellnum);
     cellnum++;
     //Entering for Class4
-    cell.setCellStyle(cellStyle);
     cell = row.createCell(cellnum);
+    cell.setCellStyle(cellStyle);
     cell.setCellValue(person.getClass4());
     sheet.autoSizeColumn(cellnum);
     cellnum++;
     //Entering for Class5
-    cell.setCellStyle(cellStyle);
     cell = row.createCell(cellnum);
+    cell.setCellStyle(cellStyle);
     cell.setCellValue(person.getClass5()); 
     sheet.autoSizeColumn(cellnum);
     cellnum++;
     //Entering for Class6
-    cell.setCellStyle(cellStyle);
     cell = row.createCell(cellnum);
+    cell.setCellStyle(cellStyle);
     cell.setCellValue(person.getClass6()); 
     sheet.autoSizeColumn(cellnum);
     cellnum++;
     //Entering for Class7
-    cell.setCellStyle(cellStyle);
     cell = row.createCell(cellnum);
+    cell.setCellStyle(cellStyle);
     cell.setCellValue(person.getClass7()); 
     sheet.autoSizeColumn(cellnum);
     cellnum++;
     //Entering for Class8
-    cell.setCellStyle(cellStyle);
     cell = row.createCell(cellnum);
+    cell.setCellStyle(cellStyle);
     cell.setCellValue(person.getClass8()); 
     sheet.autoSizeColumn(cellnum);
     cellnum++;
     //Entering for Class9
-    cell.setCellStyle(cellStyle);
     cell = row.createCell(cellnum);
+    cell.setCellStyle(cellStyle);
     cell.setCellValue(person.getClass9()); 
     sheet.autoSizeColumn(cellnum);
     cellnum++;
     //Entering for Class10
-    cell.setCellStyle(cellStyle);
     cell = row.createCell(cellnum);
+    cell.setCellStyle(cellStyle);
     cell.setCellValue(person.getClass10()); 
     sheet.autoSizeColumn(cellnum);
     cellnum++;
     //Entering for Class11
-    cell.setCellStyle(cellStyle);
     cell = row.createCell(cellnum);
+    cell.setCellStyle(cellStyle);
     cell.setCellValue(person.getClass11()); 
     sheet.autoSizeColumn(cellnum);
     cellnum++;
     //Entering for Class12
-    cell.setCellStyle(cellStyle);
     cell = row.createCell(cellnum);
+    cell.setCellStyle(cellStyle);
     cell.setCellValue(person.getClass12()); 
     sheet.autoSizeColumn(cellnum);
     cellnum++;
     //Entering for Class13
-    cell.setCellStyle(cellStyle);
     cell = row.createCell(cellnum);
+    cell.setCellStyle(cellStyle);
     cell.setCellValue(person.getClass13()); 
     sheet.autoSizeColumn(cellnum);
     cellnum++;
     //Entering for Class14
-    cell.setCellStyle(cellStyle);
     cell = row.createCell(cellnum);
+    cell.setCellStyle(cellStyle);
     cell.setCellValue(person.getClass14()); 
     sheet.autoSizeColumn(cellnum);
     cellnum++;
     //Entering for Class15
-    cell.setCellStyle(cellStyle);
     cell = row.createCell(cellnum);
+    cell.setCellStyle(cellStyle);
     cell.setCellValue(person.getClass15()); 
     sheet.autoSizeColumn(cellnum);
     cellnum++;
+//    //Test
+//    cell = row.createCell(cellnum);  
+//    cell.setCellStyle(cellStyle);
+//    Date date = new Date();
+//    
+////    SimpleDateFormat datetemp = new SimpleDateFormat("dd-mmm-yy");
+////    java.util.Date date = datetemp.parse(person.getClass1());
+////    cell.setCellValue(date.toString());
+////    cell.setCellValue(person.getClass1().toString());
+//    
+//    cell.setCellStyle(cellStyle);
+//    sheet.autoSizeColumn(cellnum);
+//    cellnum++;
     
     
   }
+  
   try {
    //Write the workbook in file system
    FileOutputStream out = new FileOutputStream(new File("EmployeeManager.xlsx"));
