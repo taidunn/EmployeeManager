96f1d632a69ae1898bb97c917e6b7a622e42e2c8
Tai Dunn <39572984+taidunn@users.noreply.github.com>
2019-11-13 13:05:33 -0500

    Create CourseNames.java

---
+++ CourseNames.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class CourseNames {
+	public static final List<String> COURSES = Arrays.asList("Course 1", "Course 2", "Course 3",
+			"Course 4", "Course 5", "Course 6", "Course 7", "Course 8", "Course 9", "Course 10",
+			"Course 11", "Course 12", "Course 13", "Course 14", "Course 15");
+}
